eeef992682e9ff2cec5a3f64660be4092f55f4a4
Diego Silva <diefersil@gmail.com>
2021-05-10 17:31:12 -0300

    Fim conceitos

--- 01_githubexplorer/src/App.jsx
+++ 01_githubexplorer/src/App.jsx
@@ -1,5 +1,12 @@
+import './styles/global.scss';
+import { RepositoryList } from "./components/RepositoryList"
+
+/**/
+
 export function App(){
-    return <h1>Testando </h1>
+    return (
+        <RepositoryList/>
+    )
 }
 
 
--- 01_githubexplorer/webpack.config.js
+++ 01_githubexplorer/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+
+
 module.exports = {
   
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
   
-  devtool: 'eval-source-map',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
 
@@ -34,7 +38,12 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
-            }
+            },
+            {
+              test: /\.scss$/,
+              exclude: /node_modules/,
+              use: ['style-loader','css-loader','sass-loader']
+          }
         ]      
 
     }
