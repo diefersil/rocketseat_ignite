f61f17887f47c68d17185433069e8d0505b58e9d
Diego Silva <diefersil@gmail.com>
2021-05-17 21:12:31 -0300

    Fase Final Typescript dependecias

---
+++ 01_githubexplorer/src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.scss';
+import { RepositoryList } from "./components/RepositoryList"
+import { Counter } from "./components/Counter";
+
+/**/
+
+export function App(){
+    return (
+        <>
+            <RepositoryList/>
+        </>
+    )
+
+}
+
+
+
