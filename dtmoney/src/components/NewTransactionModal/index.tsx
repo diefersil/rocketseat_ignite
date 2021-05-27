9ef2eeef930f4643480964baee1295dce912ca04
Diego Silva <diefersil@gmail.com>
2021-05-27 15:42:15 -0300

    update

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
 
 interface NewTransactionModalProps{
@@ -20,6 +22,23 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
             <Container>
                 <h2>Cadastrar Transação</h2>
                 <input placeholder='Título'/>
+
+                    <TransactionTypeContainer>
+                        <button type='button'>
+                            <img src={incomeImg} alt='Entradas'/>
+                            <span>Entradas</span>
+                        </button>
+
+                        <button type='button'>
+                            <img src={outcomeImg} alt='Saídas'/>
+                            <span>Saídas</span>
+                        </button>
+
+                    </TransactionTypeContainer>
+
+
+
+
                 <input type='number' placeholder='Valor'/>
                 <input placeholder='Categoria'/>
                 <button type='submit'>Cadastrar</button>
