9f1debfffd5dfbd11d2fbc8be82051c467869f04
Diego Silva <diefersil@gmail.com>
2021-05-28 20:30:58 -0300

    rows

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        { 
+          id: 1,
+          title: 'freelancer website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-05-29 09:00:00')
+        },
+        { 
+          id: 2,
+          title: 'Alugel',
+          type: 'withdraw',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-05-29 09:00:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
